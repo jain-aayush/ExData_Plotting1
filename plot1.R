46a496e4f3cb380b7e88a7e85f651d0d16d26606
Aayush Jain <aayushjain@pop-os.localdomain>
2020-05-18 15:36:43 +0530

    Fixed plot1

--- plot1.R
+++ plot1.R
@@ -4,8 +4,8 @@
 df <- read.delim('household_power_consumption.txt', sep = ';', header = TRUE, na.strings = '?', 
                  colClasses =c(rep('character',2), rep('numeric',5)))
 #convert date and time columns from Character to the respective data types
-df$Date <- as.Date(df$Date)
-df$Time <- strptime(df$Time, format = "%H:%M:%S")
+df$DateTime <- strptime(paste(df$Date, df$Time),"%d/%m/%Y %H:%M:%S")
+df <- subset(df, as.Date(DateTime) >= as.Date("2007-02-01") & as.Date(DateTime) <= as.Date("2007-02-02"))
 #open png file
 png(filename = 'plot1.png', width = 480, height = 480, units = 'px')
 #plot the histogram
